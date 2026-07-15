a64609fc918c24eb75266da4bf5c327e6bab5907
lucasweber20 <lucasweber449@gmail.com>
2026-07-15 15:34:28 -0300

    Refactor: multithread parser urls

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from urllib.parse import unquote
 import time
 
 
-HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"}
-
 parser = argparse.ArgumentParser()
 
 args = parser.add_argument("-u", "--url", help='Set url, example: -u https://example.com/?param=value', type=str)
@@ -18,12 +16,22 @@ args = parser.add_argument("-t", "--thread", help="Specify threads number, examp
 args = parser.parse_args()
 
 def main():
+    if args.thread and args.thread > 1:
+        threads = args.thread
+    else:
+        threads = 1
 
     without_duplicates = remove_duplicates(args.list)
     urls_params = check_params(without_duplicates)
-    urls_parsed = parser_urls(urls_params)
-    check_vuln(urls_parsed)
 
+    print(f"Threads = {threads}")
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+        futures = [executor.submit(parser_urls, url) for url in urls_params]
+        for future in concurrent.futures.as_completed(futures):
+            urls_parsed = future.result()
+
+    check_vuln(urls_parsed)
+    
 def remove_duplicates(file):
     file_read = open(file, encoding="utf-8").read().splitlines()
     result = list(dict.fromkeys(file_read))
@@ -42,22 +50,22 @@ def check_params(urls):
 def parser_urls(urls):
     result = []
     file_read = open("payloads.txt", encoding="utf-8").read().splitlines()
-    for url in urls:
-        try:
-            f = furl(url)
-            for payload in file_read:
-                payload = payload.replace("{domain}", f.netloc)
-                for args in f.args:
-                    f.args[args] = payload
-                else:
-                    result.append(unquote(f.url))
-        except:
-            continue
+    try:
+        f = furl(urls)
+        for payload in file_read:
+            payload = payload.replace("{domain}", f.netloc)
+            for args in f.args:
+                f.args[args] = payload
+            else:
+                result.append(unquote(f.url))
+    except:
+        pass
     return result
 
-def req_thread(url):
+def requests_urls(url):
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"}
     try:
-        req = requests.get(url, headers=HEADERS, timeout=10)
+        req = requests.get(url, headers=headers, timeout=10)
         if req.history:
             return True, url, req.url
     except:
@@ -70,7 +78,7 @@ def check_vuln(url_list):
         threads = 1
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-        futures = [executor.submit(req_thread, url) for url in url_list]
+        futures = [executor.submit(requests_urls, url) for url in url_list]
         for future in concurrent.futures.as_completed(futures):
             redirect = future.result()
             if redirect:
